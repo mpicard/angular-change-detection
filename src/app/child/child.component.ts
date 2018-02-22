0784e2ed97a8619934c1d065d359b08845d4891d
Martin Picard <martin.picard@emc.com>
2018-02-22 19:39:32 +0000

    1 - initialization

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,27 @@
+import { Component, DoCheck, Input, OnChanges, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'child',
+  template: `
+    <div>
+      child-component
+
+      value: {{ value }}
+    </div>
+  `
+})
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
+  @Input() value;
+
+  ngOnInit() {
+    console.log("%cOnInit (child)", "color: pink");
+  }
+
+  ngOnChanges() {
+    console.log("%cOnChanges (child)", "color: pink");
+  }
+
+  ngDoCheck() {
+    console.log("%cDoCheck (child)", "color: pink");
+  }
+}
